4f758da5985742c44ea62847d7c9480791cc66e8
lucsmac <lucas.macedo0821@gmail.com>
2020-08-06 16:22:28 -0300

    built tabs navigation

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import styles from './styles'
+
+const Favorites = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+}
+
+export default Favorites
